01a9c9821e62f1f6bba6c9ef23a0429b4daea274
Ryan Welcher <me@ryanwelcher.com>
2023-09-07 15:33:24 -0400

    Add the transforms plugin.

---
+++ plugins/transforms/transforms.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Plugin Name:       Transforms
+ * Requires at least: 6.1
+ * Requires PHP:      7.0
+ * Version:           0.1.0
+ * Author:            The WordPress Contributors
+ * License:           GPL-2.0-or-later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       transforms
+ *
+ * @package           block-developers-cookbook
+ */
+
+/**
+ * Registers the block using the metadata loaded from the `block.json` file.
+ * Behind the scenes, it registers also all assets so they can be enqueued
+ * through the block editor in the corresponding context.
+ *
+ * @see https://developer.wordpress.org/reference/functions/register_block_type/
+ */
+function block_developers_cookbook_transforms_block_init() {
+	register_block_type( __DIR__ . '/build' );
+}
+add_action( 'init', 'block_developers_cookbook_transforms_block_init' );
