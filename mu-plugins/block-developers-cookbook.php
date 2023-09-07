5661d721c56fa981f1bcc5a17e4d6fc8ddd07dbf
Ryan Welcher <me@ryanwelcher.com>
2023-09-07 10:18:32 -0400

    Move some features to mu-plugins

---
+++ mu-plugins/block-developers-cookbook.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Plugin Name:       Block Developers Cookbook
+ * Description:       Some global functionality for the Block Developer Cookbook
+ * Requires at least: 6.1
+ * Requires PHP:      7.0
+ * Version:           0.1.0
+ * Author:            Ryan Welcher
+ * Author URI:        www.ryanwelcher.com
+ * License:           GPL-2.0-or-later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       bdc
+ */
+
+add_filter(
+	'block_categories_all',
+	function( $categories ) {
+		array_unshift(
+			$categories,
+			array(
+				'slug'  => 'block-developers-cookbook',
+				'title' => __( 'Block Developers Cookbook', 'block-developers-cookbook' ),
+			)
+		);
+		return $categories;
+	},
+	10
+);
+
+// // Add some post meta.
+// add_action(
+// 	'init',
+// 	function() {
+// 		register_post_meta(
+// 			'post',
+// 			'firstName',
+// 			array(
+// 				'show_in_rest' => true,
+// 				'single'       => true,
+// 				'type'         => 'string',
+// 			)
+// 		);
+// 		register_post_meta(
+// 			'post',
+// 			'lastName',
+// 			array(
+// 				'show_in_rest' => true,
+// 				'single'       => true,
+// 				'type'         => 'string',
+// 			)
+// 		);
+// 	}
+// );
