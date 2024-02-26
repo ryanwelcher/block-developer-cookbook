bfb56f1ca6e1081638361f9620ef798205b42580
Ryan Welcher <me@ryanwelcher.com>
2024-02-26 10:32:49 -0500

    whitespace fix

--- plugins/iapi-gallery-slider/src/render.php
+++ plugins/iapi-gallery-slider/src/render.php
@@ -10,7 +10,7 @@
  * @see https://github.com/WordPress/gutenberg/blob/trunk/docs/reference-guides/block-api/block-metadata.md#render
  */
 
- $context = array_merge(
+$context = array_merge(
 	$attributes,
 	array(
 		'slides'       => array(),
