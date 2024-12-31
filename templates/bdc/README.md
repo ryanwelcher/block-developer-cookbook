08d28284afc4563c234995aec8e4343dd1976a67
Ryan Welcher <me@ryanwelcher.com>
2024-12-31 15:13:22 -0500

    Fix up the BDC template.

---
+++ templates/bdc/README.md
@@ -0,0 +1,13 @@
+A create-block template that I use to do all things.
+
+## Usage
+
+`npx @wordpress/create-block my block --template @ryanwelcher/all-the-things`
+
+
+## Available variants
+
+* `dynamic`
+* `static`
+* `plugin`
+* `interactive`
--- templates/bdc/index.js
+++ templates/bdc/index.js
@@ -1,27 +1,25 @@
-const { join } = require( 'path' );
+const { join } = require('path');
 
 module.exports = {
 	defaultValues: {
-		namespace: 'block-developers-cookbook',
-		category: 'block-developers-cookbook',
 		customPackageJSON: {
 			prettier: '@wordpress/prettier-config',
 		},
 		example: {},
-		wpScripts: false,
-		customPackageJSON: {
-			scripts: {
-				build: 'wp-scripts build',
-				format: 'wp-scripts format',
-				'lint:css': 'wp-scripts lint-style',
-				'lint:js': 'wp-scripts lint-js',
-				'packages-update': 'wp-scripts packages-update',
-				'plugin-zip': 'wp-scripts plugin-zip',
-				start: 'wp-scripts start',
-			},
-			devDependencies: {
-				'@wordpress/scripts': '^26.12.0',
-			},
+		transformer: (view) => {
+			const {
+				variantVars: { isPluginVariant },
+				slug,
+			} = view;
+			return {
+				...view,
+				customScripts: isPluginVariant
+					? {
+							build: `wp-scripts build ${slug}.js`,
+							start: `wp-scripts start ${slug}.js`,
+						}
+					: view.customScripts,
+			};
 		},
 	},
 	variants: {
@@ -31,16 +29,16 @@ module.exports = {
 		static: {},
 		plugin: {},
 		interactive: {
-			viewScriptModule: "file:./view.js",
+			viewScriptModule: 'file:./view.js',
 			customScripts: {
-			  build: "wp-scripts build --experimental-modules",
-			  start: "wp-scripts start --experimental-modules",
+				build: 'wp-scripts build --experimental-modules',
+				start: 'wp-scripts start --experimental-modules',
 			},
 			supports: {
-			  interactive: true,
+				interactive: true,
 			},
-		}
+		},
 	},
-	pluginTemplatesPath: join( __dirname, 'templates/plugin' ),
-	blockTemplatesPath: join( __dirname, 'templates/block' ),
+	pluginTemplatesPath: join(__dirname, 'templates/plugin'),
+	blockTemplatesPath: join(__dirname, 'templates/block'),
 };
