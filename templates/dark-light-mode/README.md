18104b9ccc6fb2a51f452b565385d99c1e97f576
Ryan Welcher <me@ryanwelcher.com>
2025-02-13 18:50:56 -0500

    Commit initial template.

---
+++ templates/dark-light-mode/README.md
@@ -0,0 +1,18 @@
+# Dark Light Mode Block Template
+
+This template provides a tutorial on how to create an interactive block that allows users to toggle between dark and light color schemes.
+
+## Usage
+
+```bash
+npx @wordpress/create-block dark-light-mode --template @block-developer-cookbook/dark-light-mode
+```
+
+## Variants
+
+- `start` - scaffolds starting files for the tutorial
+- `completed` - scaffolds the completed files for the tutorial
+
+## Tutorial
+
+The tutorial can be found in the main README.md file of the generated plugin or at [https://blockdevelopercookbook.com/recipes/creating-a-dark-light-mode-block/](https://blockdevelopercookbook.com/recipes/creating-a-dark-light-mode-block/)
---
+++ templates/dark-light-mode/index.js
@@ -0,0 +1,29 @@
+const { join } = require('path');
+const { version, description } = require('./package.json');
+
+module.exports = {
+	defaultValues: {
+		version,
+		description,
+		namespace: 'block-developers-cookbook',
+		customPackageJSON: {
+			prettier: '@wordpress/prettier-config',
+		},
+		example: {},
+		viewScriptModule: 'file:./view.js',
+		render: 'file:./render.php',
+		supports: {
+			interactive: true,
+		},
+		customScripts: {
+			build: 'wp-scripts build --experimental-modules',
+			start: 'wp-scripts start --experimental-modules',
+		},
+	},
+	variants: {
+		start: {},
+		completed: {},
+	},
+	pluginTemplatesPath: join(__dirname, 'files/plugin'),
+	blockTemplatesPath: join(__dirname, 'files/src'),
+};
