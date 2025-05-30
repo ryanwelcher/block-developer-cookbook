8d88f6268715b66d1956eef57bdccadfcd0d230d
Ryan Welcher <me@ryanwelcher.com>
2025-05-30 12:33:38 -0400

    Completed index, package.json  and readme.

---
+++ templates/meme-generator/README.md
@@ -0,0 +1,14 @@
+This template provides a tutorial to create a meme generator block.
+
+## Usage
+
+`npx @wordpress/create-block meme-generator --template @block-developer-cookbook/meme-generator`
+
+## Variants
+
+- `start` - starting files
+- `completed` - final files
+
+## Tutorial
+
+The tutorial can be found in the main README.md file of the generated plugin or at [https://blockdevelopercookbook.com/recipes/meme-generator/](https://blockdevelopercookbook.com/recipes/meme-generator/)
---
+++ templates/meme-generator/index.js
@@ -0,0 +1,91 @@
+/**
+ * Dependencies
+ */
+const { join } = require('path');
+const { version, description } = require('./package.json');
+
+module.exports = {
+	defaultValues: {
+		version,
+		description,
+		namespace: 'block-developers-cookbook',
+		customPackageJSON: {
+			prettier: '@wordpress/prettier-config',
+		},
+		category: 'design',
+		attributes: {},
+		example: {
+			attributes: {
+				topText: "I don't always meme",
+				bottomText: 'But when I do, I use the Meme Generator',
+				image: {
+					name: 'The Most Interesting Man In The World',
+					url: 'https://i.imgflip.com/1bh8.jpg',
+					width: 550,
+					height: 690,
+				},
+				style: {
+					elements: {
+						link: {
+							color: {
+								text: 'var:preset|color|base',
+							},
+						},
+					},
+					typography: {
+						textAlign: 'center',
+						fontStyle: 'normal',
+						fontWeight: '700',
+					},
+				},
+				textColor: 'base',
+				fontSize: 'x-large',
+				fontFamily: 'montserrat',
+			},
+		},
+		supports: {},
+	},
+	variants: {
+		start: {},
+		completed: {
+			attributes: {
+				topText: {
+					type: 'string',
+				},
+				bottomText: {
+					type: 'string',
+				},
+				image: {
+					type: 'object',
+				},
+			},
+			supports: {
+				html: false,
+				color: {
+					text: true,
+					background: false,
+					gradients: false,
+					enableContrastChecker: false,
+				},
+				typography: {
+					fontSize: true,
+					textAlign: true,
+				},
+				__experimentalFontFamily: true,
+				__experimentalFontStyle: true,
+				__experimentalFontWeight: true,
+			},
+			example: {
+				attributes: {
+					authorName: 'WordPress',
+				},
+			},
+			customBlockJSON: {
+				usesContext: ['postId', 'postType'],
+			},
+		},
+	},
+	pluginTemplatesPath: join(__dirname, 'files/plugin'),
+	blockTemplatesPath: join(__dirname, 'files/block'),
+	assetsPath: join(__dirname, 'files/assets'),
+};
