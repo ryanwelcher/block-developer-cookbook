a760e9e5865ec6ff206e4ae862400d5bb8f88206
Ryan Welcher <me@ryanwelcher.com>
2025-08-08 10:38:40 -0400

    Split out all of the bdc template files.

--- templates/bdc/index.js
+++ templates/bdc/index.js
@@ -15,8 +15,8 @@ module.exports = {
 				...view,
 				customScripts: isPluginVariant
 					? {
-							build: `wp-scripts build ./src/${slug}.js`,
-							start: `wp-scripts start ./src/${slug}.js`,
+							build: `wp-scripts build ${slug}.js`,
+							start: `wp-scripts start ${slug}.js`,
 						}
 					: view.customScripts,
 			};
@@ -26,11 +26,24 @@ module.exports = {
 		dynamic: {
 			render: 'file:./render.php',
 		},
-		static: {},
-		plugin: {},
+		static: {
+			blockTemplatesPath: join(
+				__dirname,
+				'templates/block/static-variant'
+			),
+		},
+		plugin: {
+			pluginTemplatesPath: join(
+				__dirname,
+				'templates/plugin/plugin-variant'
+			),
+			blockTemplatesPath: join(
+				__dirname,
+				'templates/block/plugin-variant'
+			),
+		},
 		interactive: {
 			viewScriptModule: 'file:./view.js',
-			render: 'file:./render.php',
 			customScripts: {
 				build: 'wp-scripts build --experimental-modules',
 				start: 'wp-scripts start --experimental-modules',
@@ -38,8 +51,12 @@ module.exports = {
 			supports: {
 				interactive: true,
 			},
+			blockTemplatesPath: join(
+				__dirname,
+				'templates/block/interactive-variant'
+			),
 		},
 	},
-	pluginTemplatesPath: join(__dirname, 'templates/plugin'),
-	blockTemplatesPath: join(__dirname, 'templates/block'),
+	pluginTemplatesPath: join(__dirname, 'templates/plugin/default'),
+	blockTemplatesPath: join(__dirname, 'templates/block/default'),
 };
